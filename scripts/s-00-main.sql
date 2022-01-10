78b09429901341be0e114da3c1f9bfe6728983df
Edgar Garcia <cristobalstressed115@gmail.com>
2022-01-10 11:35:20 -0600

    Terminando consultas 3

--- scripts/s-00-main.sql
+++ scripts/s-00-main.sql
@@ -3,7 +3,6 @@ Prompt Conectando a usuario gaga_proy_admin
 connect gaga_proy_admin/gaga
 @s-02-entidades.sql
 connect gaga_proy_admin/gaga
-@s-03-tablas-temporales
 Prompt primer tabla externa
 @s-04-tablas-externa-med
 Prompt segunda tabla externa
@@ -13,4 +12,5 @@ connect gaga_proy_admin/gaga
 @s-06-indices.sql
 @s-07-sinonimos.sql
 @s-08-vistas
+@s-03-tablas-temporales
 @s-09-carga-inicial.sql
