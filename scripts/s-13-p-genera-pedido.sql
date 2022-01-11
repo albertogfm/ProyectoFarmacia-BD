1292b4441568db57c171786cbe3f0bb110a6bc1b
Edgar Garcia <cristobalstressed115@gmail.com>
2022-01-11 16:09:18 -0600

    Finalizando procedimientos

--- scripts/s-13-p-genera-pedido.sql
+++ scripts/s-13-p-genera-pedido.sql
@@ -1,2 +1,64 @@
-create or replace procedure genera-pedido( )
-is 
+create or replace procedure gen(
+  v_rfc in varchar2,
+  status_pedido_id in number
+)
+is
+  v_n number(2,0);
+  v_pedido_id pedido.pedido_id%type;
+  v_folio pedido.folio%type;
+  v_fecha_status pedido.fecha_status%type
+  v_cliente cliente.cliente_id%type;
+  v_status pedido.status_pedido_id%type;
+  v_fecha_pedido = pedido.fecha_pedido%type;
+  v_importe = pedido.importe%type;
+  v_medicamento = varchar2(200);
+  v_presentancion = varchar2(2);
+  v_unidades = number(10,0);
+  v_medicamento_id medicamento.medicamento_id%type;
+  v_presentancion_id number(10,0);
+begin
+  select pedido_seq into v_pedido_id from dual; 
+  select cliente_id into v_cliente from cliente where rfc = v_rfc;
+  v_folio := DBMS_RANDOM.string('u',13);
+  v_importe:= dbms_random.value(1000,5000);
+  v_fecha_pedido := sysdate;
+  v_fecha_status := sysdate;
+  v_status := 1;
+  
+  insert into pedido (pedido_id,folio,feche_pedido,importe,
+    fecha_status,cliente_id,status_pedido_id)
+  values(v_pedido_id,v_folio,v_feche_pedido,v_importe,
+    v_fecha_status,v_cliente_id,v_status_pedido_id);
+
+
+    dbms_output.put_line("Ingrese la cantidad de medicamentos 
+      que vaya a pedir");
+    v_n := &med;
+  
+    for i in 1..v_n loop
+      dbms_output.put_line("Ingrese el medicamento que va a comprar");
+      v_medicamento := &nom;
+      dbms_output.put_line("Ingrese la clave del tamaño de la dosis");
+      dbms_output.put_line("  XS = 1 pastillas");
+      dbms_output.put_line("  S = 5 pastillas");
+      dbms_output.put_line("  M = 10 pastillas");
+      dbms_output.put_line("  G = 20 pastillas");
+      dbms_output.put_line("  XG = 3O pastillas");
+      v_presentancion = &tam;
+      dbms_output.put_line("Ingrese el numero de unidades de "||v_medicamento);
+      v_unidades= &uni;
+      select medicamento_id into v_medicamento_id 
+        from lista_nombres_medicamento where nombre=v_medicamento;
+      select presentacion_id into v_presentancion_id
+        from presentacion where clave=upper(v_presentancion);
+      insert into medicamento_presentacion(medicamento_presentacion_id,
+        presentacion_id,medicamento_id) 
+        values (medicamento_presentacion_seq.nextval,v_presentancion_id,v_medicamento_id);
+      insert into pedido_medicamento(pedido_medicamento_id,numero_unidades, 
+        pedido_id,clave_centro_operaciones,medicamento_presentacion_id)
+        values(pedido_medicamento_seq.nextval,v_unidades,v_pedido_id,'ISX4YK',
+        medicamento_presentacion_seq.currval);
+    end loop;
+  end;
+  /
+show errors
--- scripts/s-14-p-generar-nuevo-usuario-prueba.sql
+++ scripts/s-14-p-generar-nuevo-usuario-prueba.sql
@@ -1,30 +1,9 @@
 declare 
-  v_nombre cliente.nombre%type; 
-  v_apellido_paterno cliente.apellido_paterno%type; 
-  v_apellido_materno cliente.apellido_materno%type; 
-  v_rfc cliente.rfc%type;
-  v_direccion cliente.direccion%type;
-  v_telefono cliente.telefono%type;
-  v_email cliente.email%type;
-  v_numero_tarjeta cliente.numero_tarjeta%type;
-  v_anio tarjeta.anio_expiracion%type;
-  v_mes tarjeta.mes_expiracion%type; 
-  v_curp cliente.curp%type;
   v_estado number(10,0) := -1 ;
 begin
-  v_nombre := 'Ricardo';
-  v_apellido_paterno := 'Perez';
-  v_apellido_materno := 'Martinez';
-  v_rfc := '0123456789HMM'; 
-  v_direccion := 'CANDELARIA';
-  v_telefono := 5566777888;
-  v_email := 'UNCORREO@CORREO.COM';
-  v_numero_tarjeta := '0000000000000000';
-  v_anio := '23';
-  v_mes :=  '12';
-  v_curp := 'JAJE111555HMTRDA51';
 
-  createUser(v_nombre, v_apellido_paterno, v_apellido_materno, v_rfc, v_direccion, v_telefono,v_email,v_numero_tarjeta, v_anio, v_mes, v_curp,v_estado);
+  createUser('RICARDO', 'PEREZ', 'MARTINEZ', '0123456789HMM', 'CANDELARIA 581',
+  5566777888,'UNCORREO@CORREO.COM','0000000000000000', '23', '12', 'JAJE111555HMTRDA51',v_estado);
 
   if v_estado = 1 then
     dbms_output.put_line('Prueba 1 realizada exitosamente');
@@ -32,6 +11,27 @@ begin
     dbms_output.put_line('Prueba 1 no fue exitosa');
   end if;
   rollback;
+
+  createUser('GERRARDO', 'FERNANDEZ', 'GUTIERREZ', 'BFBS003649N20', 'LOS PINOS 91',
+  5566777888,'OTROCORREO@CORREO.COM','4700596613251155', '22', '1', null,v_estado);
+  if v_estado = 1 then
+    dbms_output.put_line('Prueba 1 realizada exitosamente');
+  else
+    dbms_output.put_line('Prueba 1 no fue exitosa');
+  end if;
+  rollback;
+
+
+  createUser('CRISTOBAL', 'GARCIA', 'GUTIERREZ', 'GAGE000213V05', 'TEXCOCO MUY MUY LEJANO',
+  5556697888,'CRIS@KGB.COM','4700596613251155', '25', '10', null,v_estado);
+  if v_estado = 1 then
+    dbms_output.put_line('Prueba 1 realizada exitosamente');
+  else
+    dbms_output.put_line('Prueba 1 no fue exitosa');
+  end if;
+  rollback;
+
+
 end;
 /
 show errors
